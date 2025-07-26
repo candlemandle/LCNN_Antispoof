0b65a27532955a95f410545e9cc12314310c3f9e
v01d <150617039+candlemandle@users.noreply.github.com>
2025-07-26 17:08:49 +0300

    smoke test successful

--- src/utils.py
+++ src/utils.py
@@ -3,26 +3,40 @@ import torch
 import librosa
 from torch.utils.data import Dataset
 
+
 target_map = {'bonafide': 0, 'spoof': 1}
 
 class ASVDataset(Dataset):
-    def __init__(self, root_dir, subset, feature_extractor):
-        self.root = os.path.join(root_dir, subset, 'flac')
-        self.lst = self._load_list(subset)
-        self.fe = feature_extractor
+    def __init__(self, root_dir: str, subset: str, feature_extractor):
 
-    def _load_list(self, subset):
-        key_file = os.path.join(self.root, '..', subset + '.txt')
-        lines = open(key_file).read().splitlines()
-        return [(os.path.join(self.root, utt + '.flac'), target_map[label])
-                for utt, label in (line.split() for line in lines)]
+        self.audio_dir = os.path.join(root_dir, subset, 'flac')
+
+        self.samples = self._load_list(root_dir, subset)
+        self.fe      = feature_extractor
+
+    def _load_list(self, root_dir: str, subset: str):
+
+        proto_path = os.path.join(root_dir, subset, f"{subset}.txt")
+        lines      = open(proto_path, 'r').read().splitlines()
+        samples    = []
+        for line in lines:
+            parts   = line.split()
+            file_id = parts[1]
+            label   = parts[-1]
+            samples.append((file_id, target_map[label]))
+        return samples
 
     def __len__(self):
-        return len(self.lst)
+        return len(self.samples)
 
     def __getitem__(self, idx):
-        path, label = self.lst[idx]
-        wav_np, sr = librosa.load(path, sr=None, mono=True)
-        wav = torch.from_numpy(wav_np).unsqueeze(0)  # (1, time)
+        file_id, label = self.samples[idx]
+
+        wav_path = os.path.join(self.audio_dir, file_id + '.flac')
+
+        wav_np, sr = librosa.load(wav_path, sr=None, mono=True)
+
+        wav = torch.from_numpy(wav_np).unsqueeze(0)
+
         spec = self.fe(wav)
         return spec, torch.tensor(label, dtype=torch.long)
